cf3caefc6c4602a962d9d8ddebb0a2e395bf38f9
Joseph Burger <joseph@airkit.com>
2023-11-11 16:21:46 -0600

    bug: absolute imge url for github display?

--- src/pages/ViteBoilerplate.tsx
+++ src/pages/ViteBoilerplate.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
-import reactLogo from "../assets/react.svg";
+import reactLogo from "/src/assets/react.svg";
 import { PageInterface } from "./interface";
 
 type ViteBoilerPlateProps = object;
