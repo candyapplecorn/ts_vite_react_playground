14ffd87f4e88dc0a16a727be5a17bac1e1c38b2c
Joseph Burger <joseph@airkit.com>
2023-11-16 15:09:19 -0600

    fix: swapped to hash router for github pages; added index

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./pages/error";
 import { getPagesIterator } from "./state/get-pages-iterator";
+import About from "./pages/About";
 
 getPagesIterator().then((pages) => {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
+      path: "/",
       element: <Root />,
       errorElement: <ErrorPage />,
-      children: pages.map(({ title, component: Component }) => ({
-        path: title,
-        element: <Component />,
-      })),
+      children: [
+        {
+          index: true,
+          element: <About />,
+        },
+        ...pages.map(({ title, component: Component }) => ({
+          path: title,
+          element: <Component />,
+        })),
+      ],
     },
   ]);
 
